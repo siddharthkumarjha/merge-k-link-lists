d6beb93b8b99cfe316bc82b83f8213665cfd63f2
Siddharth Kumar Jha <siddharth.k.jha@proton.me>
2025-04-19 00:33:29 +0530

    update cmake and add 1 TC

--- main.cpp
+++ main.cpp
@@ -1,15 +1,51 @@
+#include <functional>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+/*
+ * go like defer
+ * uses RAII syntax to defer operations to the end of scope
+ * defers in LIFO
+ *
+ * eg: code,
+ *
+ * defer { cleanup1 };
+ * defer { cleanup2 };
+ *
+ * executes,
+ *
+ * cleanup2
+ * cleanup1
+ */
+class defer_impl
+{
+    std::function<void(void)> m_Func;
+
+public:
+    defer_impl() = delete;
+    defer_impl(auto &&func) : m_Func(std::move(func)) {}
+    defer_impl &operator=(auto &&func)
+    {
+        m_Func = std::move(func);
+        return *this;
+    }
+
+    ~defer_impl()
+    {
+        if (m_Func)
+            m_Func();
+    }
+};
+
+#define CAT2(x, y) x##y
+#define CAT(x, y) CAT2(x, y)
+#define DEFER_OBJ_NAME CAT(defer_call_, __LINE__)
+#define defer defer_impl DEFER_OBJ_NAME = [&]() -> void
+
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
  */
-
-#include <vector>
 struct ListNode
 {
     int val;
@@ -114,4 +150,87 @@ public:
     }
 };
 
-int main() { return 0; }
+void print_linked_list(ListNode *node)
+{
+    const char *sep = "[";
+    if (node == nullptr)
+        std::cout << sep;
+
+    std::unordered_set<ListNode *> visited_nodes;
+    for (; node != nullptr && visited_nodes.find(node) == visited_nodes.end();
+         node = node->next)
+    {
+        std::cout << sep << node->val << "(" << node << ")";
+        sep = " -> ";
+        visited_nodes.insert(node);
+    }
+
+    if (node != nullptr)
+    {
+        std::cerr << "\nPANIC: Unexpected circular linked list "
+                     "detected!\nAborting printing operation."
+                  << std::endl;
+    }
+    std::cout << "]";
+}
+
+void gc(ListNode *&head)
+{
+    ListNode *cur = nullptr;
+    while (head != nullptr)
+    {
+        cur  = head;
+        head = head->next;
+        delete cur;
+        cur = nullptr;
+    }
+    head = nullptr;
+}
+
+ListNode *&add_node(ListNode *&head, int val)
+{
+    ListNode *node = new ListNode(val);
+    node->next     = head;
+    head           = node;
+    return head;
+}
+
+ListNode *make_list(std::initializer_list<int> vals)
+{
+    ListNode *head              = nullptr;
+    ListNode **head_indirection = &head;
+    for (const auto &val : vals)
+    {
+        *head_indirection = new ListNode(val);
+        head_indirection  = &((*head_indirection)->next);
+    }
+    return head;
+}
+
+int main()
+{
+    std::vector<ListNode *> lists;
+    defer
+    {
+        for (auto &node : lists)
+            gc(node);
+        lists.clear();
+    };
+
+    lists.emplace_back(make_list({1, 4, 5}));
+    lists.emplace_back(make_list({1, 3, 4}));
+    lists.emplace_back(make_list({2, 6}));
+
+    std::cout << "input to mergeKLists\n";
+    for (const auto &head : lists)
+    {
+        print_linked_list(head);
+        std::cout << std::endl;
+    }
+
+    std::cout << "\noutput\n";
+    Solution s;
+    print_linked_list(s.mergeKLists(lists));
+    std::cout << std::endl;
+    return 0;
+}
