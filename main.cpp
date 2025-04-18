237ef4c1d140d5cddb4422cd533bf52cea56b6bc
Siddharth Kumar Jha <siddharth.k.jha@proton.me>
2025-04-19 01:58:47 +0530

    improv: introduce concepts and generics

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <functional>
 #include <iostream>
+#include <iterator>
+#include <type_traits>
 #include <unordered_set>
 #include <vector>
 
@@ -25,12 +27,6 @@ class defer_impl
 public:
     defer_impl() = delete;
     defer_impl(auto &&func) : m_Func(std::move(func)) {}
-    auto operator=(auto &&func) -> defer_impl &
-    {
-        m_Func = std::move(func);
-        return *this;
-    }
-
     ~defer_impl()
     {
         if (m_Func)
@@ -46,127 +42,145 @@ public:
 /**
  * Definition for singly-linked list.
  */
-struct ListNode
+namespace list
 {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+template <typename T> struct node
+{
+    T val;
+    node *next;
+    node() : val(0), next(nullptr) {}
+    node(T x) : val(x), next(nullptr) {}
+    node(T x, node *next) : val(x), next(next) {}
 };
 
-class Solution
+template <typename> struct is_node : std::false_type
 {
-    auto splice_list(ListNode **lastNode, ListNode *newHead)
-        -> std::pair<ListNode **, size_t>
-    {
-        size_t n    = 0;
-        *(lastNode) = newHead;
-        while (*lastNode != nullptr)
-        {
-            lastNode = &((*lastNode)->next);
-            ++n;
-        }
-
-        return {lastNode, n};
-    }
-
-    template <typename Iter>
-        requires requires(Iter it) {
-            { *it } -> std::convertible_to<ListNode *>;
-        }
-    auto flatten_list(Iter list_begin, Iter list_end)
-        -> std::pair<ListNode *, size_t>
-    {
-        while (list_begin != list_end && *list_begin == nullptr)
-            ++list_begin;
-        if (list_begin == list_end)
-            return {nullptr, 0};
-
-        ListNode head{};
-        ListNode *dummy_ptr = &head;
-        ListNode **lastNode = &(dummy_ptr->next);
-        size_t n            = 0;
-
-        for (; list_begin != list_end; ++list_begin)
-        {
-            auto [tail, len] = splice_list(lastNode, *list_begin);
-            lastNode         = tail;
-            n += len;
-        }
-
-        return {head.next, n};
-    }
-
-    auto merge(ListNode *l1, ListNode *l2) -> ListNode *
-    {
-        ListNode dummy;
-        ListNode *tail = &dummy;
-
-        while (l1 != nullptr && l2 != nullptr)
-        {
-            if (l1->val < l2->val)
-            {
-                tail->next = l1;
-                l1         = l1->next;
-            }
-            else
-            {
-                tail->next = l2;
-                l2         = l2->next;
-            }
-            tail = tail->next;
-        }
-        tail->next = (l1 != nullptr) ? l1 : l2;
-        return dummy.next;
-    }
-    auto merge_sort(ListNode *head, size_t n) -> ListNode *
-    {
-        if (n <= 1)
-            return head;
-        size_t mid = n / 2;
-
-        ListNode *left  = head;
-        ListNode *right = head;
-        ListNode *prev  = nullptr;
-
-        for (size_t i = 0; i < mid; ++i)
-        {
-            prev  = right;
-            right = right->next;
-        }
-        prev->next = nullptr;
-
-        return merge(merge_sort(left, mid), merge_sort(right, n - mid));
-    }
-
-public:
-    auto mergeKLists(std::vector<ListNode *> &lists) -> ListNode *
-    {
-        if (lists.size() <= 0)
-            return nullptr;
-
-        auto [flatListHead, n] = flatten_list(lists.begin(), lists.end());
-        return merge_sort(flatListHead, n);
-    }
 };
+template <typename T> struct is_node<node<T>> : std::true_type
+{
+};
+template <typename T> inline constexpr bool is_node_v = is_node<T>::value;
 
-auto print_linked_list(ListNode *node) -> void
+namespace algorithm
+{
+template <typename T>
+auto splice_list(node<T> **lastNode, node<T> *newHead)
+    -> std::pair<node<T> **, size_t>
+{
+    size_t n    = 0;
+    *(lastNode) = newHead;
+    while (*lastNode != nullptr)
+    {
+        lastNode = &((*lastNode)->next);
+        ++n;
+    }
+
+    return {lastNode, n};
+}
+
+template <typename T>
+concept nodeItr = is_node_v<std::remove_pointer_t<std::iter_value_t<T>>>;
+
+template <nodeItr Iter> using node_ptr = std::iter_value_t<Iter>;
+
+template <nodeItr Iter>
+auto flatten_list(Iter list_begin, Iter list_end)
+    -> std::pair<node_ptr<Iter>, size_t>
+{
+    using node_t = std::remove_pointer_t<node_ptr<Iter>>;
+
+    while (list_begin != list_end && *list_begin == nullptr)
+        ++list_begin;
+    if (list_begin == list_end)
+        return {nullptr, 0};
+
+    node_t head{};
+    node_ptr<Iter> dummy_ptr = &head;
+    node_ptr<Iter> *lastNode = &(dummy_ptr->next);
+    size_t n                 = 0;
+
+    for (; list_begin != list_end; ++list_begin)
+    {
+        auto [tail, len] = splice_list(lastNode, *list_begin);
+        lastNode         = tail;
+        n += len;
+    }
+
+    return {head.next, n};
+}
+
+template <typename T> auto merge(node<T> *l1, node<T> *l2) -> node<T> *
+{
+    node<T> dummy;
+    node<T> *tail = &dummy;
+
+    while (l1 != nullptr && l2 != nullptr)
+    {
+        if (l1->val < l2->val)
+        {
+            tail->next = l1;
+            l1         = l1->next;
+        }
+        else
+        {
+            tail->next = l2;
+            l2         = l2->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = (l1 != nullptr) ? l1 : l2;
+    return dummy.next;
+}
+
+template <typename T> auto merge_sort(node<T> *head, size_t n) -> node<T> *
+{
+    if (n <= 1)
+        return head;
+    size_t mid = n / 2;
+
+    node<T> *left  = head;
+    node<T> *right = head;
+    node<T> *prev  = nullptr;
+
+    for (size_t i = 0; i < mid; ++i)
+    {
+        prev  = right;
+        right = right->next;
+    }
+    prev->next = nullptr;
+
+    return merge(merge_sort(left, mid), merge_sort(right, n - mid));
+}
+
+template <typename T>
+auto merge_k_lists(std::vector<node<T> *> &lists) -> node<T> *
+{
+    if (lists.size() <= 0)
+        return nullptr;
+
+    auto [flatListHead, n] = flatten_list(lists.begin(), lists.end());
+    return merge_sort(flatListHead, n);
+}
+}; // namespace algorithm
+
+namespace detail
+{
+template <typename T> auto print_linked_list(node<T> *head) -> void
 {
     const char *sep = "[";
-    if (node == nullptr)
+    if (head == nullptr)
         std::cout << sep;
 
-    std::unordered_set<ListNode *> visited_nodes;
-    for (; node != nullptr && visited_nodes.find(node) == visited_nodes.end();
-         node = node->next)
+    std::unordered_set<list::node<T> *> visited_nodes;
+    for (; head != nullptr && visited_nodes.find(head) == visited_nodes.end();
+         head = head->next)
     {
-        std::cout << sep << node->val << "(" << node << ")";
+        std::cout << sep << head->val << "(" << head << ")";
         sep = " -> ";
-        visited_nodes.insert(node);
+        visited_nodes.insert(head);
     }
 
-    if (node != nullptr)
+    if (head != nullptr)
     {
         std::cerr << "\nPANIC: Unexpected circular linked list "
                      "detected!\nAborting printing operation."
@@ -175,9 +189,9 @@ auto print_linked_list(ListNode *node) -> void
     std::cout << "]";
 }
 
-auto gc(ListNode *&head) -> void
+template <typename T> auto gc(node<T> *&head) -> void
 {
-    ListNode *cur = nullptr;
+    node<T> *cur = nullptr;
     while (head != nullptr)
     {
         cur  = head;
@@ -188,50 +202,51 @@ auto gc(ListNode *&head) -> void
     head = nullptr;
 }
 
-auto add_node(ListNode *&head, int val) -> ListNode *&
+template <typename T> auto add_node(node<T> *&head, int val) -> node<T> *&
 {
-    ListNode *node = new ListNode(val);
-    node->next     = head;
-    head           = node;
+    node<T> *new_node = new list::node<T>(val);
+    new_node->next    = head;
+    head              = new_node;
     return head;
 }
 
-auto make_list(std::initializer_list<int> vals) -> ListNode *
+template <typename T> auto make_list(std::initializer_list<T> vals) -> node<T> *
 {
-    ListNode *head              = nullptr;
-    ListNode **head_indirection = &head;
+    node<T> *head              = nullptr;
+    node<T> **head_indirection = &head;
     for (const auto &val : vals)
     {
-        *head_indirection = new ListNode(val);
+        *head_indirection = new node<T>(val);
         head_indirection  = &((*head_indirection)->next);
     }
     return head;
 }
+}; // namespace detail
+} // namespace list
 
 auto main() -> int
 {
-    std::vector<ListNode *> lists;
+    std::vector<list::node<int> *> lists;
     defer
     {
         for (auto &node : lists)
-            gc(node);
+            list::detail::gc(node);
         lists.clear();
     };
 
-    lists.emplace_back(make_list({1, 4, 5}));
-    lists.emplace_back(make_list({1, 3, 4}));
-    lists.emplace_back(make_list({2, 6}));
+    lists.emplace_back(list::detail::make_list({1, 4, 5}));
+    lists.emplace_back(list::detail::make_list({1, 3, 4}));
+    lists.emplace_back(list::detail::make_list({2, 6}));
 
     std::cout << "input to mergeKLists\n";
     for (const auto &head : lists)
     {
-        print_linked_list(head);
+        list::detail::print_linked_list(head);
         std::cout << std::endl;
     }
 
     std::cout << "\noutput\n";
-    Solution s;
-    print_linked_list(s.mergeKLists(lists));
+    list::detail::print_linked_list(list::algorithm::merge_k_lists(lists));
     std::cout << std::endl;
     return 0;
 }
